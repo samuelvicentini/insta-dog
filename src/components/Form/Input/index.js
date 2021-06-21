8882fda5eb8ad42a496237ec5d696a62b220758f
Samuel Vicentini <samuelvicentini@outlook.com>
2021-06-21 20:17:25 -0300

    criando useForm e forma de validar o email

--- src/components/Form/Input/index.js
+++ src/components/Form/Input/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 
 import style from './style.module.css';
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={style.wrapper}>
       <label className={style.label} htmlFor={name}>
         {label}
       </label>
-      <input className={style.input} id={name} name={name} type={type} />
-      <p className={style.error}>Error</p>
+      <input
+        className={style.input}
+        id={name}
+        name={name}
+        type={type}
+        onChange={onChange}
+        value={value}
+        onBlur={onBlur}
+      />
+      {error && <p className={style.error}>{error}</p>}
     </div>
   );
 };
