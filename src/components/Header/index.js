30a5e4d59dd872b9a3b4d7f1a9d2b8c75d76e903
Samuel Vicentini <samuelvicentini@outlook.com>
2021-06-22 20:27:23 -0300

    utilizando o contexto no header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,29 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
 
 import { ReactComponent as Dogs } from '../../Assets/dogs.svg';
 
 import style from './style.module.css';
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <header className={style.header}>
       <nav className={`${style.nav} container`}>
         <Link className={style.logo} to='/' aria-label='Dogs - Home'>
           <Dogs />
         </Link>
-        <Link className={style.login} to='/login'>
-          Login | Criar
-        </Link>
+        {data ? (
+          <Link className={style.login} to='/login'>
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={style.login} to='/login'>
+            Login | Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
