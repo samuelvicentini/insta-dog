77085e9e4705d33a984f0cc9110a19eee24c5c55
Samuel Vicentini <samuelvicentini@outlook.com>
2021-06-29 12:33:28 -0300

    estilizando o login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -7,20 +7,24 @@ import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 import { UserContext } from '../../context/UserContext';
 
+import style from './style.module.css';
+
 const Login = () => {
   const { login } = React.useContext(UserContext);
 
   if (login === true) return <Navigate to='/conta' />;
 
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<LoginForm />} />
-        <Route path='criar' element={<LoginCreate />} />
-        <Route path='perdeu' element={<LoginPasswordLost />} />
-        <Route path='resetar' element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={style.login}>
+      <div className={style.forms}>
+        <Routes>
+          <Route path='/' element={<LoginForm />} />
+          <Route path='criar' element={<LoginCreate />} />
+          <Route path='perdeu' element={<LoginPasswordLost />} />
+          <Route path='resetar' element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
