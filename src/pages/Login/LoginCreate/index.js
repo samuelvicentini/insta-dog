7c739303a415ae9c4e4ec642b428d0e68b1ad23f
Samuel Vicentini <samuelvicentini@outlook.com>
2021-06-30 13:57:19 -0300

    criando o usuario

--- src/pages/Login/LoginCreate/index.js
+++ src/pages/Login/LoginCreate/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { USER_POST } from '../../../api';
+import Button from '../../../components/Form/Button';
+import Input from '../../../components/Form/Input';
+import useForm from '../../../Hooks/useForm';
+import { UserContext } from '../../../context/UserContext';
 
 const LoginCreate = () => {
-  return <div>Login Create</div>;
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm();
+
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+  }
+
+  return (
+    <section className='animeLeft'>
+      <h1 className='title'>Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label='Usuário' type='text' name='username' {...username} />
+        <Input label='Email' type='email' name='email' {...email} />
+        <Input label='Senha' type='password' name='password' {...password} />
+        <Button>Cadastre-se</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
