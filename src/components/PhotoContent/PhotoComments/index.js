708ee4959cbc87108be488cdfbcb4241b8dca442
Samuel Vicentini <samuelvicentini@outlook.com>
2021-08-18 16:24:47 -0300

    postando comentario e vendo em tempo real

--- src/components/PhotoContent/PhotoComments/index.js
+++ src/components/PhotoContent/PhotoComments/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-const PhotoComments = () => {
-  return <div></div>;
+import { UserContext } from '../../../context/UserContext';
+import PhotoCommentsForm from '../PhotoCommentsForm';
+
+import style from './style.module.css';
+
+const PhotoComments = props => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const { login } = React.useContext(UserContext);
+
+  return (
+    <>
+      <ul>
+        {comments.map(comment => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
