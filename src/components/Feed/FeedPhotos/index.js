46af549053126444b5ee528543ca24d24890a393
Samuel Vicentini <samuelvicentini@outlook.com>
2021-08-19 16:13:46 -0300

    scroll infinito

--- src/components/Feed/FeedPhotos/index.js
+++ src/components/Feed/FeedPhotos/index.js
@@ -8,25 +8,25 @@ import Loading from '../../Helper/Loading';
 
 import style from './style.module.css';
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ setModalPhoto, user, page, setInfinite }) => {
   const { data, error, loading, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const { url, options } = PHOTOS_GET({ page, total: 6, user });
       const { response, json } = await request(url, options);
-      console.log(json);
+      if (response && response.ok && json.length < 3) setInfinite(false);
     }
 
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
   if (data)
     return (
       <ul className={`${style.feed} animeLeft`}>
-        {data.map((photo) => (
+        {data.map(photo => (
           <FeedPhotosItem
             key={photo.id}
             photo={photo}
--- src/pages/Conta/index.js
+++ src/pages/Conta/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Feed from '../../components/Feed';
+import { UserContext } from '../../context/UserContext';
 import UserHeader from './UserHeader/UserHeader';
 import UserPhotoPost from './UserPhotoPost/UserPhotoPost';
 import UserStats from './UserStats';
 
 const Conta = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <section className='container'>
       <UserHeader />
       <Routes>
-        <Route path='/' element={<Feed />} />
+        <Route path='/' element={<Feed user={data.id} />} />
         <Route path='postar' element={<UserPhotoPost />} />
         <Route path='estatisticas' element={<UserStats />} />
       </Routes>
